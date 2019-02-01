ac5f8f38f2275d5a153bddb10d74ad4c00d5d59c
Lee-Kyung-Won <kunglee2545@gmail.com>
2019-02-01 13:42:21 +0900

    25강. blog.views.post_new/post_edit 뷰에 messages 적용

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
@@ -35,6 +36,7 @@ def post_new(request):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save()
+            messages.success(request, '새 포스팅을 저장했습니다.')
             return redirect(post)  # post.get_absolute_url() => post detail
     else:
         form = PostForm()
@@ -50,6 +52,7 @@ def post_edit(request, id):
         form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save()
+            messages.success(request, '포스팅을 수정했습니다.')
             return redirect(post)  # post.get_absolute_url() => post detail
     else:
         form = PostForm(instance=post)
