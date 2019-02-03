9c4f87518df5a7a1fcba0675986746e6e8e5d736
Lee-Kyung-Won <kunglee2545@gmail.com>
2019-02-03 19:21:56 +0900

    33강. 회원가입 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def signup(request):
+    if request.method == 'Post':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
 
 
 def profile(request):
